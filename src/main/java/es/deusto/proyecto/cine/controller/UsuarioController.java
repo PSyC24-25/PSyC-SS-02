aaf5b38a07daf9fcee2c2baa79196b2f23290e8f
Gontzaa86 <gonzalo.hernandez@opendeusto.es>
2025-04-30 09:39:55 +0200

    Mejora visual en compra de tikcets Agregado recuadro para mejorar la visualización del panel de reserva

--- src/main/java/es/deusto/proyecto/cine/controller/UsuarioController.java
+++ src/main/java/es/deusto/proyecto/cine/controller/UsuarioController.java
@@ -1,16 +1,22 @@
 package es.deusto.proyecto.cine.controller;
 
-import es.deusto.proyecto.cine.dto.UsuarioDTO;
-import es.deusto.proyecto.cine.model.Usuario;
-import es.deusto.proyecto.cine.service.UsuarioService;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import es.deusto.proyecto.cine.dto.UsuarioDTO;
+import es.deusto.proyecto.cine.model.Usuario;
+import es.deusto.proyecto.cine.service.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
